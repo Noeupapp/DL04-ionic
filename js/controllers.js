d8a4a45de7bf785affb2204703a68e62d00722eb
quentin <quentin@noeupapp.com>
2017-07-12 12:33:00 +0200

    Accel page with color

--- js/controllers.js
+++ js/controllers.js
@@ -2,6 +2,43 @@ angular.module('starter.controllers', [])
 
 .controller('MotionCtrl', function($scope,$cordovaDeviceMotion) {
 
+      var options = { frequency: 500 };
+
+  document.addEventListener("deviceready", function () {
+
+    var watch = $cordovaDeviceMotion.watchAcceleration(options);
+
+        watch.then(
+          null,
+          function(error) {
+          // An error occurred
+          },
+          function(result) {
+            $scope.r = Math.floor((result.x+10)*255/20);
+            $scope.g = Math.floor((result.y+10)*255/20);
+            $scope.b = Math.floor((result.z+10)*255/20);
+            $scope.colorProp = {'background-color':"rgb("+$scope.r+","+$scope.g+","+$scope.b+")"};
+        });
+
+      }, false);
+
+
+
+
+
+
+
+      $scope.getAccel = function(){
+        $cordovaDeviceMotion.getCurrentAcceleration().then(function(result) {
+              $scope.x = result.x;
+              $scope.y = result.y;
+              $scope.z = result.z;
+
+        }, function(err) {
+          alert("Erreur")
+        });
+      }
+
 })
 
 .controller('WhereamiCtrl', function($scope) {
