a44fee885e90febcb1ef8bf51aa4cdf7ccce0f3b
quentin <quentin@noeupapp.com>
2017-07-12 11:16:45 +0200

    ionic navigation with 4 tabs

---
+++ js/controllers.js
@@ -0,0 +1,17 @@
+angular.module('starter.controllers', [])
+
+.controller('MotionCtrl', function($scope) {
+
+})
+
+.controller('WhereamiCtrl', function($scope) {
+
+})
+
+
+.controller('PostsCtrl', function($scope) {
+})
+
+.controller('LsCtrl', function($scope) {
+
+});
